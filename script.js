fbbb728f4d38bfb3b387efae78c75502aeb89a53
Hadeer_Elfiky <66886721+HadeerAIA@users.noreply.github.com>
2022-11-01 15:43:32 +0200

    this is our first files

---
+++ script.js
@@ -0,0 +1,9 @@
+const clicking = function (){
+    document.write ("Hello Hossam and Hadeer")
+}
+// const X = document.getElementsByClassName(clicking);
+// X.addEventListener("click", myScript)
+//  function myScript (){
+//     document.write("hello Hossam and Hadeer!")
+// }
+document.write("helloooooooo!!")
